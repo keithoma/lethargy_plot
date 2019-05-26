3b71c8a2f8359e5ee41c2ccf6a45d1993275a5eb
keithoma <thomakmj@gmail.com>
2019-05-26 23:06:29 +0200

    working working

--- huffman_coding/main.cpp
+++ huffman_coding/main.cpp
@@ -1,96 +1,96 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-#include <map>
-#include <iostream>
-#include <algorithm>
-#include <utility>
-#include <optional>
 
 using namespace std;
 
-struct Node
-{
-    bool has_child;
-
-    // we use the key attribute to compare the two branches
-    unsigned key;
-
-    // here, we store the frequencies of each character
-    pair<char, unsigned> data;
-};
-
-struct Tree
-{
-    Node node;
-    Tree* left;
-    Tree* right;
-    Tree* parent;
-};
-
-class huffman_encoding
+class HuffmanCoding
 {
     public:
-        huffman_encoding(string input_string);
-
-    // private:
-
-        // this function may fail; upgrade it with optional if possible
-        pair<char, unsigned> find_least_erase();
-        Tree create_parent(Tree left_branch, Tree right_branch);
-        void create_tree();
-
+        HuffmanCoding(string text);
     
     // private:
-        map<char, unsigned> frequency_;
-        vector<Tree> tree;
-        vector<bool> huffman_code_;
+    	typedef function<bool(pair<char, unsigned>, pair<char, unsigned>)> Comparator;
+	    Comparator compFunctor = [](pair<char, unsigned> elem1, pair<char, unsigned> elem2) {
+		    return elem1.second > elem2.second;
+	    };
+        map<char, unsigned> huffman_map_; // ordered by keys
+        set<pair<char, unsigned>, Comparator> huffman_set_; // ordered by value
+
+        void create_encoding_map();
+
+        unsigned cumulative_frequency_(unsigned n);
+        map<char, vector<bool>> encoding_map_;
 };
 
-huffman_encoding::huffman_encoding(string input_string) {
-    for (char& c : input_string) {
-        frequency_[c]++;
+HuffmanCoding::HuffmanCoding(string text) {
+
+    // create a map with characters as keys and the frequencies as values
+    for (char& c : text) {
+        huffman_map_[c]++;
     }
+
+    // order the above map with value and save it as "huffman_set_"
+	huffman_set_ = {huffman_map_.begin(), huffman_map_.end(), compFunctor};
 }
 
-pair<char, unsigned> huffman_encoding::find_least_erase() {
-
-    // find the iterator for the least frequent occurrence
-    auto iter = min_element(begin(frequency_), end(frequency_), [](const auto& a, const auto& b) -> bool {
-        return a.second < b.second;
-    });
-
-    // save the found pair before we erase and return
-    pair<char, unsigned> least = make_pair(iter -> first, iter -> second);
-    frequency_.erase(iter);
-    return least;
+// TODO: somehow this does not work
+/**
+ * She returns the cumulative frequency of the characters in "huffman_set_" from the end of the set to n
+ * (i.e. she should count the ascending frequencies n times). Be careful when piloting her; "huffman_set_"
+ * is in descending order, but we want to count the cumulative total from the least frequent character.
+ * 
+ * Args:
+ *  unsigned n: the "upper bound" (or in this case the lower bound) of the summation
+ * 
+ * Returns:
+ *  unsigned total: the cumulative frequency up to "n"
+ */
+unsigned HuffmanCoding::cumulative_frequency_(unsigned n) {
+    unsigned total;
+    // can certainly be optimized
+    for (unsigned i = huffman_set_.size(); i > n; --i) {
+        total += next(huffman_set_.begin(), i).second;
+        cout << total << "\n";
+    }
+    return total;
 }
 
+// TODO: hope this works, but she needs the function above
+/**
+ * She populates "encoding_map_" which contains the isomorphic map between the characters and binary.
+ */
+void HuffmanCoding::create_encoding_map() {
+    vector<bool> recycle;
+    for (pair<char, unsigned> element : huffman_set_) {
+        unsigned n = 0;
+        unsigned cum = cumulative_frequency_(n);
+        ++n;
+        if (element.second < cum) {
+            recycle.push_back(0);
+            encoding_map_.insert(pair<char, vector<bool>>(element.first, recycle));
+        } else {
+            recycle.push_back(1);
+            encoding_map_.insert(pair<char, vector<bool>>(element.first, recycle));
+        }
+    }
 
-// (T_T) binary tree is too hard
-void huffman_encoding::create_tree() {
-    // find the two most infrequent letters
-    pair<char, unsigned> very_least = find_least_erase();
-    pair<char, unsigned> second_least = find_least_erase();
-
-    // create a node for each of them
-    Tree *left_branch;
-    left_branch->node = {false, very_least.second, very_least};
-
-    Tree *right_branch;
-    right_branch->node = {false, second_least.second, second_least};
-    
-    Tree *parent_branch;
-    parent_branch->node.key = very_least.second + second_least.second;
-    parent_branch->left = left_branch;
-    parent_branch->right = right_branch;
-
-    left_branch->parent = parent_branch;
-    right_branch->parent = parent_branch;
 }
 
-int main(){
+int main() {
     const string random_string = "Two households, both alike in dignity, \nIn fair Verona, where we lay our scene, \nFrom ancient grudge break to new mutiny, \nWhere civil blood makes civil hands unclean. \nFrom forth the fatal loins of these two foes \nA pair of star-cross'd lovers take their life; \nWhose misadventured piteous overthrows \nDo with their death bury their parents' strife. \nThe fearful passage of their death-mark'd love, \nAnd the continuance of their parents' rage, \nWhich, but their children's end, nought could remove, \nIs now the two hours' traffic of our stage; \nThe which if you with patient ears attend, \nWhat here shall miss, our toil shall strive to mend.";
-    huffman_encoding new_obj(random_string);
+
+    HuffmanCoding evariste_galois(random_string);
+
+    for (pair<char, unsigned> element : evariste_galois.huffman_set_) {
+        int n = 0;
+		cout << "char: " << element.first << " freq: " << element.second << " cum: " << evariste_galois.cumulative_frequency_(n) << "\n";
+        ++n;
+    }
 
     return 0;
 }
