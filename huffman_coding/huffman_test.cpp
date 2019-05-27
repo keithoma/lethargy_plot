6c55671a2fa22abafaea1791523f100b3e521e9a
Christian Parpart <christian@parpart.family>
2019-05-27 03:25:01 +0200

    clang-format

--- huffman_coding/huffman_test.cpp
+++ huffman_coding/huffman_test.cpp
@@ -1,24 +1,23 @@
 #include <algorithm>
 #include <iterator>
 #include <map>
-#include <string>
-#include <vector>
 #include <memory>
-#include <variant>
 #include <sstream>
+#include <string>
+#include <variant>
+#include <vector>
 
 #include <iostream>
 
-namespace ranges
-{
+namespace ranges {
 
 template <typename Container, typename F>
 auto min_element(Container& container, F f)
 {
-	return std::min_element(begin(container), end(container), f);
+    return std::min_element(begin(container), end(container), f);
 }
 
-} // namespace ranges
+}  // namespace ranges
 
 using namespace std;
 
@@ -27,35 +26,33 @@ struct Leaf;
 
 using Node = variant<Branch, Leaf>;
 
-struct Leaf
-{
-	char ch;
-	unsigned frequency;
+struct Leaf {
+    char ch;
+    unsigned frequency;
 };
 
-struct Branch
-{
-	unique_ptr<Node> left;
-	unique_ptr<Node> right;
-	unsigned frequency;
+struct Branch {
+    unique_ptr<Node> left;
+    unique_ptr<Node> right;
+    unsigned frequency;
 };
 
 using HuffmanCodes = vector<pair<char, vector<bool>>>;
 
 string to_string(HuffmanCodes const& codes)
 {
-	stringstream os;
-	int i = 0;
-	for (auto const& code : codes)
-	{
-		if (i++)
-			os << ", ";
-		os << '{' << code.first << ": ";
-		for (bool bit : code.second)
-			os << (bit ? '1' : '0');
-		os << '}';
-	}
-	return os.str();
+    stringstream os;
+    int i = 0;
+    for (auto const& code : codes)
+    {
+        if (i++)
+            os << ", ";
+        os << '{' << code.first << ": ";
+        for (bool bit : code.second)
+            os << (bit ? '1' : '0');
+        os << '}';
+    }
+    return os.str();
 }
 
 /*
@@ -64,130 +61,129 @@ string to_string(HuffmanCodes const& codes)
  */
 HuffmanCodes encode(Node const& root, vector<bool> state = {})
 {
-	vector<pair<char, vector<bool>>> result;
+    vector<pair<char, vector<bool>>> result;
 
-	if (holds_alternative<Leaf>(root))
-		result.emplace_back(make_pair(get<Leaf>(root).ch, state));
-	else
-	{
-		auto const& br = get<Branch>(root);
+    if (holds_alternative<Leaf>(root))
+        result.emplace_back(make_pair(get<Leaf>(root).ch, state));
+    else
+    {
+        auto const& br = get<Branch>(root);
 
-		state.push_back(false);
-		auto x = encode(*br.left, state);
-		result.insert(end(result), begin(x), end(x));
+        state.push_back(false);
+        auto x = encode(*br.left, state);
+        result.insert(end(result), begin(x), end(x));
 
-		state.back() = true;
-		auto y = encode(*br.right, state);
-		result.insert(end(result), begin(y), end(y));
-	}
+        state.back() = true;
+        auto y = encode(*br.right, state);
+        result.insert(end(result), begin(y), end(y));
+    }
 
-	return result;
+    return result;
 }
 
 void printDotFormat(ostream& os, Node const& n)
 {
-	// TODO: print edge labels (with "0" and "1")
-	if (holds_alternative<Branch>(n))
-	{
-		auto const& br = get<Branch>(n);
+    // TODO: print edge labels (with "0" and "1")
+    if (holds_alternative<Branch>(n))
+    {
+        auto const& br = get<Branch>(n);
 
-		os << "\"-(" << br.frequency << ")\" -> ";
-		printDotFormat(os, *br.left);
+        os << "\"-(" << br.frequency << ")\" -> ";
+        printDotFormat(os, *br.left);
 
-		os << "\"-(" << br.frequency << ")\" -> ";
-		printDotFormat(os, *br.right);
-	}
-	else
-	{
-		auto const& leaf = get<Leaf>(n);
-		os << '"' << leaf.ch << "(" << leaf.frequency << ")\";\n";
-	}
+        os << "\"-(" << br.frequency << ")\" -> ";
+        printDotFormat(os, *br.right);
+    }
+    else
+    {
+        auto const& leaf = get<Leaf>(n);
+        os << '"' << leaf.ch << "(" << leaf.frequency << ")\";\n";
+    }
 }
 
 string printDotFormat(Node const& root)
 {
-	std::stringstream out;
+    std::stringstream out;
 
-	out << "digraph D {\n";
-	printDotFormat(out, root);
-	out << "}\n";
+    out << "digraph D {\n";
+    printDotFormat(out, root);
+    out << "}\n";
 
-	return out.str();
+    return out.str();
 }
 
 std::string to_string(Node const& node)
 {
-	stringstream os;
-	if (holds_alternative<Leaf>(node))
-	{
-		auto const& leaf = get<Leaf>(node);
-		os << "{" << leaf.frequency << ": " << leaf.ch << "}";
-	}
-	else
-	{
-		auto const& br = get<Branch>(node);
-		os << "{" << br.frequency << ": " << to_string(*br.left) << ", " << to_string(*br.right) << "}";
-	}
-	return os.str();
+    stringstream os;
+    if (holds_alternative<Leaf>(node))
+    {
+        auto const& leaf = get<Leaf>(node);
+        os << "{" << leaf.frequency << ": " << leaf.ch << "}";
+    }
+    else
+    {
+        auto const& br = get<Branch>(node);
+        os << "{" << br.frequency << ": " << to_string(*br.left) << ", " << to_string(*br.right) << "}";
+    }
+    return os.str();
 }
 
 Node huffman_encode(string const& data)
 {
-	if (data.empty())
-		return {};
+    if (data.empty())
+        return {};
 
-	auto static const smallestFreq = [](auto const& a, auto const& b) { return a.second < b.second; };
+    auto static const smallestFreq = [](auto const& a, auto const& b) { return a.second < b.second; };
 
-	// collect frequencies
-	map<char, unsigned> freqs;
-	for_each(begin(data), end(data), [&](char c) { freqs[c]++; });
+    // collect frequencies
+    map<char, unsigned> freqs;
+    for_each(begin(data), end(data), [&](char c) { freqs[c]++; });
 
-	// create initial root
-	Node root = [&]()
-	{
-		auto a = ranges::min_element(freqs, smallestFreq);
-		auto a_ = Leaf{a->first, a->second};
-		freqs.erase(a);
+    // create initial root
+    Node root = [&]() {
+        auto a = ranges::min_element(freqs, smallestFreq);
+        auto a_ = Leaf{a->first, a->second};
+        freqs.erase(a);
 
-		auto b = ranges::min_element(freqs, smallestFreq);
-		if (b == end(freqs))
-			return Node{a_};
+        auto b = ranges::min_element(freqs, smallestFreq);
+        if (b == end(freqs))
+            return Node{a_};
 
-		auto b_ = Leaf{b->first, b->second};
-		freqs.erase(b);
-		return Node{Branch{make_unique<Node>(a_), make_unique<Node>(b_), a_.frequency + b_.frequency}};
-	}();
+        auto b_ = Leaf{b->first, b->second};
+        freqs.erase(b);
+        return Node{Branch{make_unique<Node>(a_), make_unique<Node>(b_), a_.frequency + b_.frequency}};
+    }();
 
-	if (freqs.empty())
-		return move(root);
+    if (freqs.empty())
+        return move(root);
 
-	// eliminate remaining frequencies and build tree bottom-up
-	// NB: It is guaranteed that root is a Branch (not a Leaf).
-	do
-	{
-		auto a = ranges::min_element(freqs, smallestFreq);
-		Leaf a_{a->first, a->second};
-		auto const frequency = a_.frequency + get<Branch>(root).frequency;
-		freqs.erase(a);
-		root = a_.frequency < get<Branch>(root).frequency
-			? Branch{make_unique<Node>(a_), make_unique<Node>(move(root)), frequency}
-			: Branch{make_unique<Node>(move(root)), make_unique<Node>(a_), frequency};
-	} while (!freqs.empty());
+    // eliminate remaining frequencies and build tree bottom-up
+    // NB: It is guaranteed that root is a Branch (not a Leaf).
+    do
+    {
+        auto a = ranges::min_element(freqs, smallestFreq);
+        Leaf a_{a->first, a->second};
+        auto const frequency = a_.frequency + get<Branch>(root).frequency;
+        freqs.erase(a);
+        root = a_.frequency < get<Branch>(root).frequency
+                   ? Branch{make_unique<Node>(a_), make_unique<Node>(move(root)), frequency}
+                   : Branch{make_unique<Node>(move(root)), make_unique<Node>(a_), frequency};
+    } while (!freqs.empty());
 
-	return move(root);
+    return move(root);
 }
 
 int main(int argc, const char* argv[])
 {
-	Node const root = huffman_encode("aaaabcadaaabbaaaacaabaadc");
+    Node const root = huffman_encode("aaaabcadaaabbaaaacaabaadc");
 
-	cout << to_string(root) << "\n\n";
+    cout << to_string(root) << "\n\n";
 
-	// print in dot file format (see graphviz)
-	cout << printDotFormat(root) << "\n";
+    // print in dot file format (see graphviz)
+    cout << printDotFormat(root) << "\n";
 
-	HuffmanCodes const codes = encode(root);
-	cout << to_string(codes) << "\n";
+    HuffmanCodes const codes = encode(root);
+    cout << to_string(codes) << "\n";
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
